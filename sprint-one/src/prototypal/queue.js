9dcb8ae4046ecf9219c3399f1c9446ea084b6a3b
Huan Tran <huanductran98@gmail.com>
2022-06-05 15:19:25 -0700

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    var storageKeysList = Object.keys(this.storage);
+    this.storage[storageKeysList.length] = value;
+  },
+  dequeue: function() {
+    var storageKeysList = Object.keys(this.storage);
+    var storageValuesList = Object.values(this.storage);
+    var dequeuedValue = storageValuesList[0];
+    delete this.storage[storageKeysList.length - 1];
+    storageKeysList.pop();
+    storageValuesList.shift();
+    for (var i = 0; i < storageKeysList.length; i++) {
+      this.storage[i] = storageValuesList[i];
+    }
+    return dequeuedValue;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    var objectLength = Object.keys(this.storage);
+    this.storage[objectLength.length] = value;
+  },
+  pop: function() {
+    var objectLength = Object.keys(this.storage);
+    var lastValue = this.storage[objectLength.length - 1];
+    delete this.storage[objectLength.length - 1];
+    return lastValue;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+
+};
 
 
