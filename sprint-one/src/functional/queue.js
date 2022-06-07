053e7338016ab1951c379d82c4cf3590b22dfa90
Yui1002 <yuimurayama1002@gmail.com>
2022-06-06 23:51:20 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,37 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add a value to the back of the queue
+    var size = someInstance.size();
+
+    storage[size] = value;
   };
 
+
   someInstance.dequeue = function() {
+    // Remove and return the string at the front of the queue
+
+    var keys = Object.keys(storage);
+    var deleted = storage[keys[0]];
+
+    delete storage[keys[0]];
+
+    for (var key in storage) {
+      var value = storage[key];
+      storage[key - 1] = value;
+      delete storage[key];
+    }
+    return deleted;
   };
 
   someInstance.size = function() {
+    var size = Object.entries(storage).length;
+    return size;
   };
 
   return someInstance;
 };
+
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = someInstance.size();
+
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var size = someInstance.size();
+    var deleted = storage[size - 1];
+
+    delete storage[size - 1];
+    return deleted;
   };
 
   someInstance.size = function() {
+    var entries = Object.entries(storage).length;
+
+    return entries;
   };
 
   return someInstance;
