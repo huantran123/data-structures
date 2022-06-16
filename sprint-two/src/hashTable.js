db185df3c2c23915101f9d63bbb5191b98fafdb2
Yui1002 <yuimurayama1002@gmail.com>
2022-06-15 20:33:27 -0700

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,52 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+
+  if (this._storage.get(index) === undefined) {
+    var bucket = [tuple];
+    this._storage.set(index, bucket);
+  } else {
+    var bucket = this._storage.get(index);
+
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push(tuple);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insertion  O(1)
+  retrieve O(1)
+  remove O(1)
  */
 
 
